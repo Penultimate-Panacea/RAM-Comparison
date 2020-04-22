183cef56b909e6b0126caf7df410ad42bd453b04
Penultimate-Panacea <rian+github@fantozzi.org>
2020-04-22 08:01:38 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RAM-Comparison',
+    version='1',
+    packages=[''],
+    url='https://github.com/Penultimate-Panacea/RAM-Comparison',
+    license='LGPL-3.0',
+    author='Rian Fantozzi',
+    author_email='rian+github@fantozzi.org ',
+    description='GUI driven, sortable RAM speed comparison tool'
+)
